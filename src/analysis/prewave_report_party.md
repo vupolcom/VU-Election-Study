df72db9f6e24189745a93b8a3dc66668c075c392
Wouter van Atteveldt <wouter@vanatteveldt.com>
2021-02-09 01:17:35 +0100

    Add more variables

--- src/analysis/prewave_report_party.md
+++ src/analysis/prewave_report_party.md
@@ -6,6 +6,7 @@ Wouter van Atteveldt, Nel Ruigrok, Mariken van der Velden
   - [Media Use](#media-use)
   - [Vertrouwen](#vertrouwen)
   - [Corona](#corona)
+  - [Politiek](#politiek)
 
 Leeswijzer: alle figuren zijn staafdiagrammen waarbij de volle staaf de
 waarde is voor zwevende kiezers, en de omtrek de gemiddelde is van de
@@ -17,7 +18,7 @@ representatieve steekproef van de populatie gezien worden)
 
 # Demographics
 
-![](figures/wave0_demo-1.png)<!-- -->![](figures/wave0_demo-2.png)<!-- -->![](figures/wave0_demo-3.png)<!-- -->![](figures/wave0_demo-4.png)<!-- -->![](figures/wave0_demo-5.png)<!-- -->
+![](figures/wave0_demo-1.png)<!-- -->![](figures/wave0_demo-2.png)<!-- -->![](figures/wave0_demo-3.png)<!-- -->![](figures/wave0_demo-4.png)<!-- -->![](figures/wave0_demo-5.png)<!-- -->![](figures/wave0_demo-6.png)<!-- -->
 
 # Media Use
 
@@ -35,10 +36,43 @@ representatieve steekproef van de populatie gezien worden)
 
 ![](figures/wave0_I7-1.png)<!-- -->
 
+![](figures/wave0_I9-1.png)<!-- -->
+
+| medium                                                                                                                               | gemiddeld | zwevend |   diff |
+| :----------------------------------------------------------------------------------------------------------------------------------- | --------: | ------: | -----: |
+| I feel exhausted by the amount of news coming at me                                                                                  |       2.3 |     2.5 |   0.20 |
+| I try to avoid political news                                                                                                        |       1.5 |     1.6 |   0.17 |
+| If I follow the news, it has a negative effect on my mood                                                                            |       2.2 |     2.3 |   0.12 |
+| The often explicit images in the news coming at me bother me                                                                         |       1.8 |     1.9 |   0.05 |
+| I cannot trust that the news coming at me is truthful                                                                                |       2.4 |     2.4 |   0.04 |
+| I try to avoid news about Corona                                                                                                     |       1.6 |     1.6 | \-0.04 |
+| The news takes up too much of my time                                                                                                |       1.6 |     1.6 | \-0.07 |
+| If I follow the news, it has a negative physical effect on me (e.g., I can’t keep up with the news. Headaches, sleep problems, etc.) |       1.3 |     1.3 | \-0.08 |
+| If I would talk about the news, I get in trouble with others and I prefer to avoid it                                                |       1.4 |     1.3 | \-0.13 |
+
 # Vertrouwen
 
 ![](figures/wave0_trust-1.png)<!-- -->![](figures/wave0_trust-2.png)<!-- -->
 
+| medium                                                                                                            | gemiddeld | zwevend |   diff |
+| :---------------------------------------------------------------------------------------------------------------- | --------: | ------: | -----: |
+| Against their better judgment, politicians promise more than they can deliver.                                    |       3.8 |     3.8 |   0.04 |
+| The political parties are only interested in my vote and not in my opinion.                                       |       3.3 |     3.4 |   0.04 |
+| The most important political decisions should be made by the people and not by politicians.                       |       3.0 |     3.0 |   0.02 |
+| Politicians are able to solve the most important problems.                                                        |       2.9 |     3.0 |   0.02 |
+| You become a member of parliament more by your political friends than by your skills.                             |       3.1 |     3.1 |   0.00 |
+| Politicians don’t care about the opinions of people like me.                                                      |       3.2 |     3.2 | \-0.01 |
+| Most politicians are skilled people who know what they are doing.                                                 |       3.1 |     3.0 | \-0.04 |
+| What in politics is called ‘making compromises’ is actually just betraying your principles.                       |       3.0 |     3.0 | \-0.06 |
+| A strong head of government is good for the Netherlands, even if he stretches the rules a bit to get things done. |       3.4 |     3.4 | \-0.07 |
+| I feel well represented by Dutch politicians.                                                                     |       2.8 |     2.7 | \-0.14 |
+
 # Corona
 
 ![](figures/wave0_covid-1.png)<!-- -->![](figures/wave0_covid-2.png)<!-- -->
+
+# Politiek
+
+![](figures/wave0_politics-1.png)<!-- -->![](figures/wave0_politics-2.png)<!-- -->![](figures/wave0_politics-3.png)<!-- -->![](figures/wave0_politics-4.png)<!-- -->
+
+![](figures/wave0_politics2-1.png)<!-- -->![](figures/wave0_politics2-2.png)<!-- -->![](figures/wave0_politics2-3.png)<!-- -->![](figures/wave0_politics2-4.png)<!-- -->
--- src/lib/create_labels.R
+++ src/lib/create_labels.R
@@ -43,6 +43,27 @@ lbl$E1 <- tibble(value = c(1:14,997:999),
                  label = c("CDA", "ChristenUnie", "D66", "Denk", "Forum for Democracy", "GroenLinks", "PvdA", "Animal Rights Party", "PVV", "SGP", "SP", "VVD", "50Plus Party", "Other party", "I don't know", "I didn't vote", "I wasn't eligble to vote"), 
                  wording = c("CDA", "ChristenUnie", "D66", "Denk", "Forum voor Democratie", "GroenLinks", "PvdA", "Partij voor de Dieren", "PVV", "SGP", "SP", "VVD", "50Plus Partij", "Andere partij", "Weet ik niet meer", "Ik heb niet gestemd", "Ik mocht niet stemmen"))
 
+lbl$F1 = tibble(value = c(1:10),
+       `label` = c("Politicians don't care about the opinions of people like me.", 
+                            "The political parties are only interested in my vote and not in my opinion.", 
+                            "Against their better judgment, politicians promise more than they can deliver.", 
+                            "You become a member of parliament more by your political friends than by your skills.", 
+                            "Most politicians are skilled people who know what they are doing.", 
+                            "Politicians are able to solve the most important problems.", 
+                            "I feel well represented by Dutch politicians.", 
+                            "What in politics is called 'making compromises' is actually just betraying your principles.", 
+                            "A strong head of government is good for the Netherlands, even if he stretches the rules a bit to get things done.", 
+                            "The most important political decisions should be made by the people and not by politicians."),
+       `wording` = c("Politici maken zich niet  druk om de mening van mensen zoals ik.", 
+                           "De politieke partijen zijn alleen maar geïnteresseerd in mijn stem en niet in mijn mening.",
+                           "Tegen beter weten in beloven politici meer dan ze kunnen waarmaken.", 
+                           "Kamerlid word je eerder door je politieke vrienden dan door je bekwaamheden.", 
+                           "De meeste politici zijn bekwame mensen die weten wat ze doen.", 
+                           "Politici zijn in staat om de belangrijkste problemen op te lossen.", 
+                           "Ik voel mij goed vertegenwoordigd door Nederlandse politici.", 
+                           "Wat men in de politiek ‘het sluiten van compromissen’ noemt, is eigenlijk gewoon het verraden van je principes.", 
+                           "Een sterke regeringsleider is goed voor Nederland, ook als deze de regels wat oprekt om dingen voor elkaar te krijgen.",
+                           "De belangrijkste politieke beslissingen moeten worden genomen door het volk en niet door politici."))
 
 lbl$F2 = tibble(value=c(1:11),
                 label = c("the jurisprudence", "journalism", "the government", "the House of Representatives", "the European Union", "political parties", "politicians", "Dutch democracy", "science", "big corporations", "banks"),
@@ -51,6 +72,10 @@ lbl$F2 = tibble(value=c(1:11),
 lbl$H3 = tibble(value = c(1:5), 
                      label = c("Much stricter measures to combat the virus (curfew, area ban).", "Slightly stricter measures to prevent viruses.", "The measures chosen by the government.", "Slightly more flexible measures so that only the most vulnerable elderly are protected.", "Much smoother measures to get the economy and nightlife going immediately."), 
                      wording = c("Veel strengere maatregelen om het virus tegen te gaan (avondklok, gebiedsverbod).", "Iets strengere maatregelen om virus tegen te gaan.","De maatregelen die de regering gekozen heeft.", "Iets soepelere maatregelen zodat alleen de meest kwetsbare ouderen afgeschermd worden.", "Veel soepelere maatregelen om de economie en het uitgaansleven onmiddellijk op gang te brengen."))
+
+lbl$H6 = tibble(value= c(1:3),
+            label = c("Children who are born now get it better than we do.", "The arrival of asylum seekers and immigrants is good for the Netherlands.", "International trade is good for the Netherlands."),
+            wording = c("Kinderen die nu geboren worden krijgen het beter dan wij.", "De komst van asielzoekers en immigranten is goed voor Nederland.", "Internationale handel is goed voor Nederland."))
 lbl$H7 = tibble(value = c(1:4), 
             label = c("Definitely do it", "Do it", "Don't do it", "Definitely don't do it"), 
             wording = c("Zeer zeker doen", "Doen", "Niet doen", "Zeer zeker niet doen"))
@@ -85,8 +110,23 @@ lbl$I7 <- tibble(value = c(1:2,11,3:10),
             wording = c("Nu", "NOS", "Teletekst", "Telegraaf", "AD", "NRC", "Volkskrant", "News aggregator zoals feedly of flipboard", "Apple News", "Google News",  "Anders, namelijk:"))
 
 lbl$I8 = tibble(value=c(1:13),
-label = c("NOS journaal", "RTL nieuws", "SBS (Hart van Nederland)", "Zondag met Lubach", "NU.nl", "Algemeen Dagblad", "De Telegraaf", "De Volkskrant", "NRC",	"GeenStijl", "De Correspondent", "Twitter", "Facebook"),
-wording = c("NOS journaal", "RTL nieuws", "SBS (Hart van Nederland)", "Zondag met Lubach", "NU.nl", "Algemeen Dagblad", "De Telegraaf", "De Volkskrant", "NRC",	"GeenStijl", "De Correspondent", "Twitter", "Facebook"))
+  label = c("NOS journaal", "RTL nieuws", "SBS (Hart van Nederland)", "Zondag met Lubach", "NU.nl", "Algemeen Dagblad", "De Telegraaf", "De Volkskrant", "NRC",	"GeenStijl", "De Correspondent", "Twitter", "Facebook"),
+  wording = c("NOS journaal", "RTL nieuws", "SBS (Hart van Nederland)", "Zondag met Lubach", "NU.nl", "Algemeen Dagblad", "De Telegraaf", "De Volkskrant", "NRC",	"GeenStijl", "De Correspondent", "Twitter", "Facebook"))
+
+
+lbl$I9 = tibble(value = c(1:9),
+                       label = c("I feel exhausted by the amount of news coming at me", "If I follow the news, it has a negative effect on my mood", "I cannot trust that the news coming at me is truthful", "The often explicit images in the news coming at me bother me", "If I would talk about the news, I get in trouble with others and I prefer to avoid it", "The news takes up too much of my time", "If I follow the news, it has a negative physical effect on me (e.g., I can't keep up with the news. Headaches, sleep problems, etc.)", "I try to avoid political news", "I try to avoid news about Corona"),
+                       wording = c("Ik voel me uitgeput door de hoeveelheid nieuws die op mij afkomt", "Als ik het nieuws volg, heeft dat een negatief effect op mijn humeur","Ik kan er niet op vertrouwen dat het nieuws dat op mij afkomt waarheidsgetrouw is", "De vaak expliciete beelden in het nieuws dat op mij afkomt storen me", "Als ik zou praten over het nieuws, krijg ik moeilijkheden met anderen en dat vermijd ik liever", "Het nieuws neemt te veel van mijn tijd in beslag", "Als ik het nieuws volg, heeft dat een negatief lichamelijk effect op mij (bijv. hoofdpijn, slaapproblemen, etc.)", "Ik probeer politiek nieuws te vermijden",	"Ik probeer nieuws over Corona te vermijden"))
+
+lbl$G1 = tibble(value = c(1:5),
+                label = c("How the current cabinet has governed the past 4 years", "How the VVD has ruled the past 4 years", "How the CDA has ruled the past 4 years", "How D66 has ruled the past 4 years", "How the Christian Union has ruled in the past 4 years"),
+                wording = c("Hoe het huidige kabinet de afgelopen 4 jaar heeft geregeerd", "Hoe de VVD de afgelopen 4 jaar heeft geregeerd",	"Hoe het CDA de afgelopen 4 jaar heeft geregeerd", "Hoe D66 de afgelopen 4 jaar heeft geregeerd", "Hoe de ChristenUnie de afgelopen 4 jaar heeft geregeerd"))
+
+lbl$G2 = tibble(value = c(1:13),
+       label = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"),
+       wording = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"))
+lbl$G3 = lbl$G2
+lbl$G4 = lbl$G2
 
 labels = bind_rows(lbl, .id="variable") 
 
@@ -109,8 +149,9 @@ recode_parties = function(p) case_when(
 long_mc = function() {
   d <- read_csv(here("data/intermediate/VUElectionPanel2021_wave0.csv")) 
   result = list()
-  for (var in c("F2", str_c("I", 1:8))) 
-    result[[var]] = d %>% select(iisID, starts_with(var), -contains("_other")) %>% pivot_longer(-iisID, names_to="medium") %>% 
+  
+  for (var in c("F1", "F2", str_c("G", 1:4), "H6", str_c("I", 1:9))) 
+    result[[var]] = d %>% select(iisID, starts_with(var), -contains("_other"), -G1_b) %>% pivot_longer(-iisID, names_to="medium") %>% 
     mutate(medium=label(as.numeric(str_remove(medium, glue("^{var}_"))), var)) %>% filter(!is.na(value))
   bind_rows(result, .id="question")
 }
