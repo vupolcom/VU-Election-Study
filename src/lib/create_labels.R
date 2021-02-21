bb93506114e7116973b64d137ce7056e7f9c5ff8
MarikenvdVelden <m.a.c.g.vander.velden@vu.nl>
2021-02-21 13:16:08 +0100

    NRC week 2

--- src/lib/create_labels.R
+++ src/lib/create_labels.R
@@ -2,6 +2,7 @@ library(glue)
 library(tidyverse)
 lbl = list()
 
+## Background Variables
 lbl$age = tibble(value = 1:6, 
                  label = c("<24", "25-34",  "35-44",  "45-54",  "55-64", ">64"), 
                  wording = c("18 - 110", " ", " ", " ", " ", " "))
@@ -31,18 +32,23 @@ lbl$internet_use <- tibble(value = c(1:5),
                            label = c("Much more for work", "More for work", "Equal for work and private reasons", "More for private reasons", "Much more for private reasons"), 
                            wording = c("Veel vaker voor werk", "Iets vaker voor werk", "Ongeveer evenveel voor werk als om persoonlijke redenen", "Iets vaker voor persoonlijke reden", "Veel vaker voor persoonlijke redenen"))
 
+
+## Block A
 lbl$A2 <- tibble(value = c(1:15,999), 
             label = c("CDA", "ChristenUnie", "D66", "Denk", "Forum for Democracy", "GroenLinks", "PvdA", "Animal Rights Party", "PVV", "SGP", "SP", "VVD", "50Plus Party", "Other party", "I vote blanco","I don't know yet"), 
             wording = c("CDA", "ChristenUnie", "D66", "Denk", "Forum voor Democratie", "GroenLinks", "PvdA", "Partij voor de Dieren", "PVV", "SGP", "SP", "VVD", "50Plus Partij", "Andere partij", "Ik stem blanco","Weet ik niet nog niet"))
 
+# Block B
 lbl$B2 = tibble(value = c(1:13),
         label = c("CDA & Wopke Hoekstra", "ChristenUnie & Gert-Jan Segers", "D66 & Sigrid Kaag", "Denk & Farid Azarkan", "Forum voor Democratie & Thierry Baudet", "GroenLinks & Jesse Klaver", "PvdA & Lodewijk Asscher", "Partij voor de Dieren & Esther Ouwehand", "PVV & Geert Wilders", "SGP  & Kees van der Staaij", "SP & Lilian Marijnissen", "VVD & Mark Rutte", "50Plus Partij & Liane den Haan"),
         wording = c("CDA & Wopke Hoekstra", "ChristenUnie & Gert-Jan Segers", "D66 & Sigrid Kaag", "Denk & Farid Azarkan", "Forum voor Democratie & Thierry Baudet", "GroenLinks & Jesse Klaver", "PvdA & Lodewijk Asscher", "Partij voor de Dieren & Esther Ouwehand", "PVV & Geert Wilders", "SGP  & Kees van der Staaij", "SP & Lilian Marijnissen", "VVD & Mark Rutte", "50Plus Partij & Liane den Haan"))
 
+# Block E
 lbl$E1 <- tibble(value = c(1:14,997:999), 
                  label = c("CDA", "ChristenUnie", "D66", "Denk", "Forum for Democracy", "GroenLinks", "PvdA", "Animal Rights Party", "PVV", "SGP", "SP", "VVD", "50Plus Party", "Other party", "I don't know", "I didn't vote", "I wasn't eligble to vote"), 
                  wording = c("CDA", "ChristenUnie", "D66", "Denk", "Forum voor Democratie", "GroenLinks", "PvdA", "Partij voor de Dieren", "PVV", "SGP", "SP", "VVD", "50Plus Partij", "Andere partij", "Weet ik niet meer", "Ik heb niet gestemd", "Ik mocht niet stemmen"))
 
+# Block F
 lbl$F1 = tibble(value = c(1:10),
        `label` = c("Politicians don't care about the opinions of people like me.", 
                             "The political parties are only interested in my vote and not in my opinion.", 
@@ -69,6 +75,18 @@ lbl$F2 = tibble(value=c(1:11),
                 label = c("The justice system", "Journalism", "The government", "The House of Representatives", "The European Union", "Political parties", "Politicians", "Dutch democracy", "Science", "Big corporations", "Banks"),
                 wording = c("de rechtspraak","de journalistiek", "de regering", "de Tweede Kamer", "de Europese Unie", "politieke partijen", "politici", "de Nederlandse democratie", "de wetenschap", "grote bedrijven", "banken"))
 
+#Block G
+lbl$G1 = tibble(value = c(1:5),
+                label = c("How the current cabinet has governed the past 4 years", "How the VVD has ruled the past 4 years", "How the CDA has ruled the past 4 years", "How D66 has ruled the past 4 years", "How the Christian Union has ruled in the past 4 years"),
+                wording = c("Hoe het huidige kabinet de afgelopen 4 jaar heeft geregeerd", "Hoe de VVD de afgelopen 4 jaar heeft geregeerd",	"Hoe het CDA de afgelopen 4 jaar heeft geregeerd", "Hoe D66 de afgelopen 4 jaar heeft geregeerd", "Hoe de ChristenUnie de afgelopen 4 jaar heeft geregeerd"))
+
+lbl$G2 = tibble(value = c(1:13),
+                label = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"),
+                wording = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"))
+lbl$G3 = lbl$G2
+lbl$G4 = lbl$G2
+
+# Block H
 lbl$H3 = tibble(value = c(1:5), 
                      label = c("Much stricter measures to combat the virus (curfew, area ban).", "Slightly stricter measures to prevent viruses.", "The measures chosen by the government.", "Slightly more flexible measures so that only the most vulnerable elderly are protected.", "Much smoother measures to get the economy and nightlife going immediately."), 
                      wording = c("Veel strengere maatregelen om het virus tegen te gaan (avondklok, gebiedsverbod).", "Iets strengere maatregelen om virus tegen te gaan.","De maatregelen die de regering gekozen heeft.", "Iets soepelere maatregelen zodat alleen de meest kwetsbare ouderen afgeschermd worden.", "Veel soepelere maatregelen om de economie en het uitgaansleven onmiddellijk op gang te brengen."))
@@ -81,6 +99,7 @@ lbl$H7 = tibble(value = c(1:4),
             wording = c("Zeer zeker doen", "Doen", "Niet doen", "Zeer zeker niet doen"))
 
 
+# Block I 
 lbl$I1 = tibble(value = c(1:9),
               label = c("Television", "Newspapers or opinion magazines (paper or online)", "Radio (including podcast and online)", "Online news sites (such as nu.nl) or blogs", "Social media (such as Facebook, Twitter)", "Messaging apps (such as Whatsapp, messenger)", "(Offline) conversations with people", "News apps or push messages on your phone", "Search engines (such as Google or Bing)"),
               wording = c("Televisie", "Kranten of opiniebladen (op papier of online)", "Radio (inclusief podcast en online)", "Online nieuwssites (zoals nu.nl) of blogs", "Social media (bijvoorbeeld Facebook, Twitter)", "Messaging apps (bijvoorbeeld Whatsapp, messenger)", "(Offline) gesprekken met mensen", "Nieuwsapps of pushberichten op uw telefoon", "Zoekmachines (zoals Google of Bing)"))
@@ -118,15 +137,7 @@ lbl$I9 = tibble(value = c(1:9),
                        label = c("I feel exhausted by the amount of news coming at me", "If I follow the news, it has a negative effect on my mood", "I cannot trust that the news coming at me is truthful", "The often explicit images in the news coming at me bother me", "If I would talk about the news, I get in trouble with others and I prefer to avoid it", "The news takes up too much of my time", "If I follow the news, it has a negative physical effect on me (e.g., I can't keep up with the news. Headaches, sleep problems, etc.)", "I try to avoid political news", "I try to avoid news about Corona"),
                        wording = c("Ik voel me uitgeput door de hoeveelheid nieuws die op mij afkomt", "Als ik het nieuws volg, heeft dat een negatief effect op mijn humeur","Ik kan er niet op vertrouwen dat het nieuws dat op mij afkomt waarheidsgetrouw is", "De vaak expliciete beelden in het nieuws dat op mij afkomt storen me", "Als ik zou praten over het nieuws, krijg ik moeilijkheden met anderen en dat vermijd ik liever", "Het nieuws neemt te veel van mijn tijd in beslag", "Als ik het nieuws volg, heeft dat een negatief lichamelijk effect op mij (bijv. hoofdpijn, slaapproblemen, etc.)", "Ik probeer politiek nieuws te vermijden",	"Ik probeer nieuws over Corona te vermijden"))
 
-lbl$G1 = tibble(value = c(1:5),
-                label = c("How the current cabinet has governed the past 4 years", "How the VVD has ruled the past 4 years", "How the CDA has ruled the past 4 years", "How D66 has ruled the past 4 years", "How the Christian Union has ruled in the past 4 years"),
-                wording = c("Hoe het huidige kabinet de afgelopen 4 jaar heeft geregeerd", "Hoe de VVD de afgelopen 4 jaar heeft geregeerd",	"Hoe het CDA de afgelopen 4 jaar heeft geregeerd", "Hoe D66 de afgelopen 4 jaar heeft geregeerd", "Hoe de ChristenUnie de afgelopen 4 jaar heeft geregeerd"))
 
-lbl$G2 = tibble(value = c(1:13),
-       label = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"),
-       wording = c("Wopke Hoekstra", "Gert-Jan Segers", "Sigrid Kaag", "Farid Azarkan",  "Thierry Baudet","Jesse Klaver", "Lodewijk Asscher", "Esther Ouwehand", "Geert Wilders", "Kees van der Staaij","Lilian Marijnissen", "Mark Rutte", "Liane den Haan"))
-lbl$G3 = lbl$G2
-lbl$G4 = lbl$G2
 
 write_csv(labels, here("data/raw/codebook.csv"))
 
