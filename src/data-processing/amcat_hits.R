4d13f2febd7b6a7b5a61679279e023c00ed13acc
Wouter van Atteveldt <wouter@vanatteveldt.com>
2021-03-22 19:00:14 +0100

    Upodate

--- src/data-processing/amcat_hits.R
+++ src/data-processing/amcat_hits.R
@@ -34,13 +34,24 @@ query_lijsttrekker = function(naam, partij) {
 clean = function(x) x %>% str_replace_all("[“”]", '"')
 rsplit = function(x) str_match(x, ".* (.*)")[,2]
 
-## Query definitions
+## Sets
 
-parties= read_csv(here("data/raw/partijen.csv")) %>% mutate(across(everything(), clean))
-issues = readxl::read_xlsx(here("data/raw/210320_v20.xlsx"), sheet="issue") 
+sets = c(Newspapers=2562, Online=2564, TV=2579)
+
+## Metadata
+meta = purrr::map(sets, function(set) amcat.articles(conn, project=69, articleset=set, columns = c("date", "publisher"))) %>% 
+  bind_rows(.id="mtype") %>% 
+  as_tibble() %>%
+  mutate(id=as.numeric(id))%>% 
+  filter(date < "2021-03-18", date >= "2021-01-01") %>%
+  anti_join(brieven)
+
+
 
 ## Build queries
 
+parties= read_csv(here("data/raw/partijen.csv")) %>% mutate(across(everything(), clean))
+issues = readxl::read_xlsx(here("data/raw/210320_v20.xlsx"), sheet="issue") 
 queries = bind_rows(
   parties %>% mutate(cat="actor", subcat=partij, query=query_party(partij, alias)) %>% select(cat, subcat, label=partij, query),
   parties %>% mutate(cat="actor", subcat=partij, query=query_lijsttrekker(lijsttrekker, partij)) %>% select(cat, subcat, label=lijsttrekker, query),
@@ -51,35 +62,6 @@ queries = bind_rows(
 
 conn = amcatr::amcat.connect("https://vu.amcat.nl")
 
-h = amcat.hits(conn, project=69, sets=c(2562, 2564, 2579), queries=queries$query, labels = queries$label) 
-
-query_brieven = '(title:brieven title:bbc title:"geachte redactie" title:"geachte lezer" title:lezersbrieven title:lezersreacties title:kruiswoord* title:"tv-ladder" tite:correctie)'
+h = amcat.hits(conn, project=69, sets=c(2562, 2564, 2579), queries=queries$query, labels = queries$label)
 
 
-
-meta_print = amcat.articles(conn, project=69, articleset=2562, columns = c("date", "publisher")) %>%
-  as_tibble() %>%  add_column(mtype="Newspapers")
-meta_online = amcat.articles(conn, project=69, articleset=2564, columns = c("date", "publisher")) %>%
-  as_tibble() %>%  add_column(mtype="Online (nos.nl/nu.nl)") %>% filter(publisher %in% c("NOS nieuws", "NU.nl"))
-meta_tv = amcat.articles(conn, project=69, articleset=2579, columns = c("date", "publisher")) %>% 
-  as_tibble() %>% add_column(mtype="TV")
-meta = bind_rows(meta_print, meta_online, meta_tv) %>%
-  mutate(id=as.numeric(id)) %>%
-  filter(date < "2021-03-18", date >= "2021-01-01")
-
-
-write_rds(meta, tmp)
-}
-tmp = here("data/tmp/wave2_media_visibility.rmd")
-if (file.exists(tmp)) {
-  d = read_rds(tmp)
-} else {
-  conn = amcat.connect("https://vu.amcat.nl")
-  h = amcat.hits(conn, project=69, sets=c(2562, 2564, 2579), queries=queries$query, labels = queries$partij) %>% 
-    as_tibble() %>% rename(party=query)
-  d = left_join(h, meta)
-  write_rds(d, tmp)
-}
-
-totals = meta %>% group_by(period, mtype) %>% summarize(ntot=n())
-parties = d %>% group_by(party) %>% summarize(n=n()) %>% arrange(-n) %>% pull(party)
